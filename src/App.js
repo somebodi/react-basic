14b9b0019bc6e73d595b1659ae8c83968fc9ad04
Cameron Murphy <cem1of3@gmail.com>
2021-12-30 16:41:06 -0600

    Add azure function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      console.log("fetching data");
+      const response = await fetch(`/api/getmessage?name=cameron`);
+      console.log(response);
+      const message = await response.json();
+      console.log(message);
+      setData(message);
+    })();
+  });
+
+  return <div><h1>test:</h1>{data}</div>;
 }
 
 export default App;
